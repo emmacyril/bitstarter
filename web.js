0e9c1de11eafd3a677fb873b9c722423141e41f5
emmacyril <emmacyril@gmail.com>
2013-07-25 03:57:48 +0100

    Update on ChildLift Organization

---
+++ web.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express.createServer(express.logger());
+var fs = require('fs');
+
+app.get('/', function(request, response) {
+  var buffer  = fs.readFileSync('index.html');
+  response.send(buffer.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
